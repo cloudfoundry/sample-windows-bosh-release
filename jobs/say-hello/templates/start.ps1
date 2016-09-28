f4dbe84d9916aa7f6f333f7c56e27b8f28d84ab1
Ben Moss <bmoss@pivotal.io>
2016-09-28 11:08:05 -0400

    rename 'cck' repro job to 'install-windows-features'
    Signed-off-by: Charlie Vieth <cviethjr@pivotal.io>

--- jobs/say-hello/templates/start.ps1
+++ jobs/say-hello/templates/start.ps1
@@ -1,6 +1,5 @@
 while ($true)
 {
+    Write-Host "I am executing a BOSH JOB 1, ${Env:FOO_1}!!!!!!!!!!!"
 	Start-Sleep 1.0
-	Write-Host $Env:FOO_1
-	Write-Host "I am executing a BOSH JOB 1, BRUH!!!!!!!!!!!"
 }
