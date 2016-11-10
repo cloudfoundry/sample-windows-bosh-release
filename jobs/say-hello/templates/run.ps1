595ca573c4d3cf6a13706a32ec512f59493d1320
Natalie Arellano <narellano@pivotal.io>
2016-11-10 09:39:16 -0500

    WIP: I don't know why these changes are heregit add -A
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

--- jobs/say-hello/templates/run.ps1
+++ jobs/say-hello/templates/run.ps1
@@ -1,4 +1,6 @@
-cmd.exe /c 'reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /s'
-Get-Content C:\windows\panther\unattendGC\*.log
-Sart-Sleep -Seconds 10
+while ($true) {
+    Write-Host "Hello, Windows!!!"
+    Start-Sleep -Seconds 5
+}
+
 Exit 0
